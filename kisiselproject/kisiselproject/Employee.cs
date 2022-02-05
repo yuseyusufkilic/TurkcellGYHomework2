6a580bf4b1b857e1bcb9363ac0bf9ea88a5b1ab2
Yuşe Yusuf Kılıç <79745654+yuseyusufkilic@users.noreply.github.com>
2022-02-05 19:37:18 +0300

    Open-Closed Principle example performed.
    One of the SOLID principles have performed for Turkcell Bootcamp Homework.

---
+++ kisiselproject/kisiselproject/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kisiselproject
+{
+
+    public abstract class Employee
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public double MainWage { get; set; } = 5000;
+
+        public double LevelBonus { get; set; }
+
+        public abstract double CalculateWage();
+    }
+
+    public class SeniorDev : Employee
+    {
+        public override double CalculateWage()
+        {
+
+            return  (MainWage * LevelBonus);
+        }
+    }
+
+
+    public class MidDev : Employee
+    {
+        public override double CalculateWage()
+        {
+            return (MainWage * LevelBonus);
+        }
+    }
+
+    public class JuniorDev : Employee
+    {
+        public override double CalculateWage()
+        {
+            return (MainWage * LevelBonus);
+        }
+    }
+
+
+
+}
---
+++ kisiselproject/kisiselproject/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace kisiselproject
+{
+    class Program
+    {
+
+        /*
+         Rank'a göre maaş hesaplaması. 
+        
+        */
+        static void Main(string[] args)
+        {
+
+            var wages = new List<Employee>
+            {
+
+            new SeniorDev { UserID = 1, Name = "Türkay Ürkmez", LevelBonus = 2.8 },
+            new MidDev { UserID = 2, Name = "Yusuf Kükrer", LevelBonus = 1.8 },
+            new JuniorDev { UserID = 3, Name = "Yuşe Yusuf Kılıç", LevelBonus = 1.3 }
+            };
+
+            foreach (var wage in wages)
+            {
+                Console.WriteLine($"{wage.Name}'s wage is {wage.CalculateWage()}TL. ");
+            }
+
+
+
+
+
+        } 
+           
+    }
+}
